41877f1e52f77c2e7b4374f8b013f9d937b15616
everinlove <62162606+everinlove@users.noreply.github.com>
2024-08-04 15:27:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import requests
 
 # 目标URL
-#url = 'https://www.zoomeye.org/searchResult?q=udpxy+subdivisions:%E5%8C%97%E4%BA%AC&page=1'
 url = 'https://www.zoomeye.org/searchResult?q=udpxy'
 
 # 发送GET请求
@@ -9,7 +8,8 @@ response = requests.get(url)
 
 # 检查请求是否成功
 if response.status_code == 200:
-    # 打印网页内容
-    print(response.text)
+    # 将网页内容写入到res.txt文件中
+    with open('res.txt', 'w', encoding='utf-8') as file:
+        file.write(response.text)
 else:
     print(f"请求失败，状态码：{response.status_code}")
